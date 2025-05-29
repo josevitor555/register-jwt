28a13e25c1f52bc4d791f141cb333fa8ae41c8e6
josevitor555 <koservitor123@gmail.com>
2025-05-28 21:52:50 -0300

    28/05/2025

--- react-auth/src/App.jsx
+++ react-auth/src/App.jsx
@@ -1,22 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Welcome from "./components/Welcome";
 
 const App = () => {
-
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/welcome" element={<Welcome />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
 export default App;
